bbe3982329dc1e0b5570b4ab901f9961aec64e7d
Murillo Gomes Yonamine <murillo.gyonamine@gmail.com>
2024-10-13 12:25:37 -0300

    FEAT: Os manipulators foram adicionados e seus estilos também.

--- Assets/Editor/Dialogue System/Windows/DSGraphView.cs
+++ Assets/Editor/Dialogue System/Windows/DSGraphView.cs
@@ -1,21 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 using UnityEngine.UIElements;
 
-namespace TaxiDays
+namespace TaxiDays.Windows
 {
-    public class DSGraphView : GraphView
+    public class DSGraphView : GraphView // Essa classe é responsável por criar a view do grafo de diálogo
     {
-        public DSGraphView() => AddGridBackground();
-        private void AddGridBackground()
+        public DSGraphView() // Construtor da classe
+        {
+            AddManipulators();
+            AddGridBackground();
+            AddStyles();
+        }
+        private void AddManipulators() // Método que adiciona os manipuladores na view do grafo
+        {
+            SetupZoom(ContentZoomer.DefaultMinScale, ContentZoomer.DefaultMaxScale);
+            this.AddManipulator(new ContentDragger());
+        }
+        private void AddGridBackground() // Método que adiciona o fundo quadriculado na view do grafo
         {
             GridBackground gridBackground = new GridBackground();
 
             gridBackground.StretchToParentSize();
 
             Insert(0, gridBackground);
+        }
+        private void AddStyles() // Método que adiciona o estilo da view do grafo
+        {
+            StyleSheet styleSheet = (StyleSheet)EditorGUIUtility.Load("Dialogue System/DSGraphViewStyles.uss");
+            styleSheets.Add(styleSheet);
         }
     }
 }
