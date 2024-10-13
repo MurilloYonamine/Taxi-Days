53541ab27f8310b36bcca4a0facd396ca0254849
Murillo Gomes Yonamine <murillo.gyonamine@gmail.com>
2024-10-13 12:25:37 -0300

    FEAT: Os manipulators foram adicionados e seus estilos também.

--- Assets/Editor/Dialogue System/Windows/DSEditorWindow.cs
+++ Assets/Editor/Dialogue System/Windows/DSEditorWindow.cs
@@ -6,19 +6,28 @@ namespace TaxiDays.Windows
     public class DSEditorWindow : EditorWindow // Essa classe é responsável por criar a janela de edição do diálogo
     {
         [MenuItem("Window/DS/Dialogue Graph")]
-        public static void ShowExample()
+        public static void ShowExample() // Método que cria a janela de edição do diálogo
         {
             GetWindow<DSEditorWindow>("Dialogue Graph");
         }
 
-        private void OnEnable() => AddGraphView();
-        private void AddGraphView()
+        private void OnEnable() // Método que é chamado quando a janela é aberta
+        {
+            AddGraphView();
+            AddStyle();
+        }
+        private void AddGraphView() // Método que adiciona a view do grafo de diálogo na janela
         {
             DSGraphView graphView = new DSGraphView();
 
             graphView.StretchToParentSize();
 
             rootVisualElement.Add(graphView);
+        }
+        private void AddStyle() // Método que adiciona o estilo da janela
+        {
+            StyleSheet styleSheet = (StyleSheet)EditorGUIUtility.Load("Dialogue System/DSVariables.uss");
+            rootVisualElement.styleSheets.Add(styleSheet);
         }
     }
 }
