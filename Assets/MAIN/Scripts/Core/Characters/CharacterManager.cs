4e7127781d958657bee1ee617082bc4598e49c43
Murillo Gomes Yonamine <murillo.gyonamine@gmail.com>
2024-10-23 20:14:02 -0300

    FEAT: Renaming Characters when they spawn.

--- Assets/MAIN/Scripts/Core/Characters/CharacterManager.cs
+++ Assets/MAIN/Scripts/Core/Characters/CharacterManager.cs
@@ -17,8 +17,9 @@ namespace CHARACTERS
 
         private const string CHARACTER_CASTING_ID = " as ";
         private const string CHARACTER_NAME_ID = "<charname>";
-        private string characterRootPath => $"Characters/{CHARACTER_NAME_ID}";
-        private string characterPrefabPath => $"{characterRootPath}/Character - [{CHARACTER_NAME_ID}]";
+        public string characterRootPathFormat => $"Characters/{CHARACTER_NAME_ID}";
+        public string characterPrefabNameFormat => $"Character - [{CHARACTER_NAME_ID}]";
+        public string characterPrefabPathFormat => $"{characterRootPathFormat}/{characterPrefabNameFormat}";
 
         [SerializeField] private RectTransform _characterPanel = null;
         public RectTransform characterPanel => _characterPanel;
@@ -92,10 +93,10 @@ namespace CHARACTERS
 
         private GameObject GetPrefabForCharacter(string characterName)
         {
-            string prefabPath = FormatCharacterPath(characterPrefabPath, characterName);
+            string prefabPath = FormatCharacterPath(characterPrefabPathFormat, characterName);
             return Resources.Load<GameObject>(prefabPath);
         }
-        private string FormatCharacterPath(string path, string characterName) => path.Replace(CHARACTER_NAME_ID, characterName);
+        public string FormatCharacterPath(string path, string characterName) => path.Replace(CHARACTER_NAME_ID, characterName);
         private Character CreateCharacterFromInfo(CHARACTER_INFO info)
         {
             CharacterConfigData config = info.config;
