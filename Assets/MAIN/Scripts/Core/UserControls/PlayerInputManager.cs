e029f89965edec0f629506cb6089f71bc1bd828e
Murillo Gomes Yonamine <murillo.gyonamine@gmail.com>
2024-10-31 19:26:24 -0300

    FEAT: Finished input manager.

--- Assets/MAIN/Scripts/Core/UserControls/PlayerInputManager.cs
+++ Assets/MAIN/Scripts/Core/UserControls/PlayerInputManager.cs
@@ -1,23 +1,33 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 namespace DIALOGUE
 {
     public class PlayerInputManager : MonoBehaviour
     {
-        // Start is called before the first frame update
-        void Start()
+        private PlayerInput input;
+        private List<(InputAction action, Action<InputAction.CallbackContext> command)> actions = new List<(InputAction, Action<InputAction.CallbackContext>)>();
+        private void Awake()
         {
-
+            input = GetComponent<PlayerInput>();
+            InitializeActions();
         }
-
-        // Update is called once per frame
-        void Update()
+        private void InitializeActions()
         {
-            if (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.Return)) PromptAdvance();
+            actions.Add((input.actions["Next"], OnNext));
         }
-        public void PromptAdvance()
+        private void OnEnable()
+        {
+            foreach (var InputAction in actions) InputAction.action.performed += InputAction.command;
+        }
+        private void OnDisable()
+        {
+            foreach (var InputAction in actions) InputAction.action.performed -= InputAction.command;
+        }
+        public void OnNext(InputAction.CallbackContext context)
         {
             DialogueSystem.instance.OnUserPrompt_Next();
         }
