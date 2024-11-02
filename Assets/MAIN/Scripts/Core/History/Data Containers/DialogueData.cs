8597683206bce1bbc2e5543ba26487329d983fd4
Murillo Gomes Yonamine <murillo.gyonamine@gmail.com>
2024-11-02 13:52:50 -0300

    FEAT: Initialized capturing history states.

---
+++ Assets/MAIN/Scripts/Core/History/Data Containers/DialogueData.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueData : MonoBehaviour
+{
+    /*
+    Contains all data for the dialogue
+    at this point in time.
+    */
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/MAIN/Scripts/Core/History/HistoryState.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HistoryState : MonoBehaviour
+{
+    /*
+    A snapshot of data at a given
+    time in the visual novel
+    */
+    
+}
