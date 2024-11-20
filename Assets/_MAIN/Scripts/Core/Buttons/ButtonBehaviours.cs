2b38cec2d8f700952fa480417e7c36b06c200a34
Murillo Gomes Yonamine <murillo.gyonamine@gmail.com>
2024-11-19 22:02:03 -0300

    Style: Deixei os botoões do meio do jogo mais bonitos.

--- Assets/_MAIN/Scripts/Core/Buttons/ButtonBehaviours.cs
+++ Assets/_MAIN/Scripts/Core/Buttons/ButtonBehaviours.cs
@@ -9,21 +9,25 @@ public class ButtonBehaviours : MonoBehaviour, IPointerEnterHandler, IPointerExi
 {
     private static ButtonBehaviours selectedButton = null;
     public Animator anim;
+    private CanvasGroup containerCanvasGroup => GetComponentInChildren<CanvasGroup>();
 
     public void OnPointerExit(PointerEventData eventData)
     {
+        containerCanvasGroup.alpha = 0.5f;
         anim.Play("Exit");
     }
 
     public void OnPointerEnter(PointerEventData eventData)
     {
-
         if (selectedButton != null && selectedButton != this)
         {
             selectedButton.OnPointerExit(null);
         }
 
         anim.Play("Enter");
+
+        containerCanvasGroup.alpha = 1;
+
         selectedButton = this;
     }
 }
